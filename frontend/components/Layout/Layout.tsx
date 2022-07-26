b089310c106f6d68fc7d1827cf2e5f26807e7a15
KEENTO <handball0321@gmail.com>
2022-07-25 20:44:06 -0700

    Clean up Hero

--- frontend/components/Layout/Layout.tsx
+++ frontend/components/Layout/Layout.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Nav from "../nav/Nav";
-import Container from "./Container";
+import { LayoutProps } from "../../models/model";
 
-const Layout = () => {
+const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Nav />
+      <div className="container">
+        <section
+          className={`fixed top-0 left-0 container-wrapper w-full h-96 rounded-b-xl bg-common bg-cover bg-center flex justify-center items-center`}
+        >
+          <div className="fixed top-96 left-0 w-full px-5">{children}</div>
+        </section>
+      </div>
     </>
   );
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,54 +1,29 @@
 import type { NextPage } from "next";
 import Nav from "../components/nav/Nav";
 import { Fragment } from "react";
-import Container from "../components/Layout/Container";
+import Layout from "../components/Layout/Layout";
 
 const Home: NextPage = () => {
   return (
-    <Fragment>
-      <Nav />
-      <Container>
-        <section className="hero-body pt-6">
-          <div className="hero-title">
-            <h1 className="text-4xl font-bold tracking-tighter">Gathering</h1>
-            <h3 className="text-2xl font-bold tracking-tighter pt-2">
-              - Connect Together -
-            </h3>
-          </div>
-          <div className="hero-description py-8 tracking-tight">
-            <p className="text-lg">Find and interact with new people</p>
-            <p className="text-lg">Explore now to make your life better.</p>
-          </div>
-          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-            <span className="relative px-4 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Get started
-            </span>
-          </button>
-        </section>
-      </Container>
-      {/* <div className="hero">
-        <section
-          className={`hero-image w-full h-96 rounded-b-xl bg-common bg-cover bg-center flex justify-center items-center`}
-        ></section>
-        <section className="hero-body px-5 pt-6">
-          <div className="hero-title">
-            <h1 className="text-4xl font-bold tracking-tighter">Gathering</h1>
-            <h3 className="text-2xl font-bold tracking-tighter pt-2">
-              - Connect Together -
-            </h3>
-          </div>
-          <div className="hero-description py-8 tracking-tight">
-            <p className="text-lg">Find and interact with new people</p>
-            <p className="text-lg">Explore now to make your life better.</p>
-          </div>
-          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-            <span className="relative px-4 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Get started
-            </span>
-          </button>
-        </section>
-      </div> */}
-    </Fragment>
+    <Layout>
+      <section className="hero-body pt-6">
+        <div className="hero-title">
+          <h1 className="text-4xl font-bold tracking-tighter">Gathering</h1>
+          <h3 className="text-2xl font-bold tracking-tighter pt-2">
+            - Connect Together -
+          </h3>
+        </div>
+        <div className="hero-description py-8 tracking-tight">
+          <p className="text-lg">Find and interact with new people</p>
+          <p className="text-lg">Explore now to make your life better.</p>
+        </div>
+        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+          <span className="relative px-4 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            Get started
+          </span>
+        </button>
+      </section>
+    </Layout>
   );
 };
 
