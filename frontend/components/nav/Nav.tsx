1ac6677cda36dc1c8baa7ec47f116c1f62ff6287
KEENTO <handball0321@gmail.com>
2022-07-25 20:32:21 -0700

    Fix Hero

--- frontend/components/nav/Nav.tsx
+++ frontend/components/nav/Nav.tsx
@@ -8,7 +8,7 @@ const Nav = () => {
 
   return (
     <header className="fixed top-0 left-0 w-full z-10">
-      <div className="header-container px-4 py-4 flex flex-row justify-between items-center">
+      <div className="header-container px-5 py-4 flex flex-row justify-between items-center">
         <section className="header-left">
           <Link href="/">
             <span className="text-red-500 font-bold">Gathering</span>
