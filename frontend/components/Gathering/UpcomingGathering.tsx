3d21edb1e8a48bae5222772f5046ae2ee88021eb
KEENTO <handball0321@gmail.com>
2022-08-06 12:03:27 -0700

    Fix errors

--- frontend/components/Gathering/UpcomingGathering.tsx
+++ frontend/components/Gathering/UpcomingGathering.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import GatheringCard from "../Card/GatheringCard";
 
 const UpcomingGathering = () => {
-  return <ul className="py-6 text-xl font-bold">Upcoming Gathering</ul>;
+  return <ul className="py-4 text-xl font-bold">Upcoming Gathering</ul>;
 };
 
 export default UpcomingGathering;
