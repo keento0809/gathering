fa85421e3a184a3f411faa8ce994b6e655b3dfd5
KEENTO <handball0321@gmail.com>
2022-08-08 19:57:37 -0700

    Connect MongoDB

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,18 +1,59 @@
 const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const { bcrypt } = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { nextTick } = require("process");
 
-router.use("/login", (req, res) => {
-  const { email, password } = req.body;
-});
+const saltRounds = 10;
 
-router.use("/signup", body("password").isLength({ min: 6 }), (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+router.use(
+  "/login",
+  body("password").isLength({ min: 6 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    // Find the corresponded user
+    const loggedInUser =
+      // Check hash password
+      bcrypt.compare(password);
+    // const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    let token;
+
+    try {
+      token = await jwt.sign(email, process.env.TOKEN_SECRET, {
+        expiresIn: "180000",
+      });
+
+      // add userData to DB
+
+      res.status(200).json(token);
+    } catch (err) {
+      console.log(err);
+      res.status(400);
+      throw new Error("Error! Something went wrong.");
+      // return next(error);
+    }
   }
-  const { username, email, password } = req.body;
-  console.log(username, email, password);
-});
+);
+
+router.use(
+  "/signup",
+  body("password").isLength({ min: 6 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { username, email, password } = req.body;
+    console.log(username, email, password);
+  }
+);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 const userRoutes = require("./routes/UserRoutes.js");
 const gatheringRoutes = require("./routes/GatheringRoutes");
+const authRoutes = require("./routes/AuthRoutes");
 const port = process.env.PORT || 8080;
 const cookieParser = require("cookie-parser");
+
+// Connect DB
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
