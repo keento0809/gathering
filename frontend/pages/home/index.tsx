38fd58e2d8706067f098af717bc4769b7d0853e8
KEENTO <handball0321@gmail.com>
2022-08-26 20:30:15 -0700

    Fix Layout and add Head in it

--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import MyGathering from "../../components/Gathering/MyGathering";
 import UpcomingGathering from "../../components/Gathering/UpcomingGathering";
-import Layout from "../../components/Layout/Layout";
 import SearchInput from "../../components/SearchInput/SearchInput";
 
 const Home = () => {
@@ -9,7 +8,7 @@ const Home = () => {
     console.log("logging out");
   };
   return (
-    <Layout>
+    <>
       <div className="fixed top-28 left-0 w-full px-5">
         <div className="home-title">
           <h3 className="text-xl text-red-500 font-bold">Welcome user!</h3>
@@ -17,10 +16,9 @@ const Home = () => {
         <SearchInput />
         <MyGathering />
         <UpcomingGathering />
-        {/* temporary */}
         <button onClick={handleLogout}>Logout</button>
       </div>
-    </Layout>
+    </>
   );
 };
 
