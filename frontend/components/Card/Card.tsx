ad9005b9021d4e1e3a570ff943da28fe47dad62d
KEENTO <handball0321@gmail.com>
2022-09-10 10:15:30 -0700

    Fix Card.tsx

--- frontend/components/Card/Card.tsx
+++ frontend/components/Card/Card.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ChildrenProps } from "../../models/model";
 
 const Card = ({ children }: ChildrenProps) => {
+  const [minHeight, setMinHeight] = useState(500);
+  const [maxHeight, setMaxHeight] = useState(580);
+  useEffect(() => {
+    if (window.innerHeight < 700) {
+      setMaxHeight(360);
+      setMinHeight(300);
+    }
+  }, []);
   return (
-    <div className="rounded-xl bg-slate-400 min-h-500 max-h-580 p-6 mt-6 overflow-scroll">
+    <div
+      className="rounded-xl bg-slate-400 p-6 mt-6 overflow-scroll"
+      style={{ minHeight: `${minHeight}px`, maxHeight: `${maxHeight}px` }}
+    >
       {children}
     </div>
   );
