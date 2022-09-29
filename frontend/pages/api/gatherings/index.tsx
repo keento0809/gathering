f94628d6495d5e35e6faa4c2ae44eea1119c8770
KEENTO <handball0321@gmail.com>
2022-09-28 20:36:34 -0700

    Fix the order of gatherings based on date

--- frontend/pages/api/gatherings/index.tsx
+++ frontend/pages/api/gatherings/index.tsx
@@ -19,6 +19,7 @@ export default async function handler(
       const allGatherings = await db
         .collection("gatherings")
         .find({})
+        .sort({ date: -1 })
         .toArray();
       res.status(200).json(allGatherings);
     }
