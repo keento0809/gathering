62512e4aa693bfcf9e3d6f313220e693cd98ee57
KEENTO <handball0321@gmail.com>
2022-09-30 11:07:53 -0700

    Introduce Image element from next/image

---
+++ frontend/components/Modal/CardModal.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CardModal = () => {
+  return (
+    <div className="absolute top-0 right-0 bottom-0 left-0 bg-slate-700 rounded-lg z-40 opacity-80">
+      <div className="modal-container text-red-500 flex justify-center items-center h-full">
+        <p className="text-2xl font-semibold">This gathering is expired.</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardModal;
