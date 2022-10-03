b53ef73a4a09622b1ffe61d1242873a22544c7ac
KEENTO <handball0321@gmail.com>
2022-10-03 15:21:42 -0700

    Wrote About page

--- frontend/pages/about/index.tsx
+++ frontend/pages/about/index.tsx
@@ -6,32 +6,47 @@ const About = () => {
   return (
     <>
       <Wrapper>
-        <h2 className="text-2xl pl-0.5 font-bold tracking-tighter text-left text-red-500 dark:text-red-400">
+        <h2 className="text-2xl pl-2 font-bold tracking-tighter text-left text-red-500 dark:text-red-400">
           About
         </h2>
-        <div className="explanations py-8">
-          <section className="pb-8">
-            <h3 className="section-title text-lg font-bold">
-              What is Gathering?
-            </h3>
-            <p className="pt-2">Gathering is ...</p>
-          </section>
-          <section className="pb-8">
-            <h3 className="section-title text-lg font-bold">
-              How to join Gathering?
-            </h3>
-            <p className="pt-2">The way to join a gathering is ...</p>
-          </section>
-          <section className="pb-8">
-            <h3 className="section-title text-lg font-bold">
-              How to cancel Gathering?
-            </h3>
-            <p className="pt-2">
-              If you cannot make a gathering, you have to directly contact to
-              ...
-            </p>
-          </section>
-        </div>
+        <Card>
+          <div className="explanations py-2">
+            <section className="pb-12">
+              <h3 className="section-title text-lg font-extrabold">
+                What is Gathering?
+              </h3>
+              <p className="pt-4 font-medium">
+                Gathering is a social media platform for organizing in-person
+                meetup events. You can join the "gathering", which stands for an
+                event on this application, to explore your social network or
+                escape from your comfort zone, and so on. Join our gatherings
+                today to broaden your horizon!
+              </p>
+            </section>
+            <section className="pb-12">
+              <h3 className="section-title text-lg font-extrabold">
+                How to join Gathering?
+              </h3>
+              <p className="pt-4 font-medium">
+                The way to join a gathering is to select a gathering that you
+                want to join by pressing "More" button, and then fill out the
+                information (name, email, and TwitterID). After that, press
+                "Join" button to complete the application for gathering. That's
+                it!
+              </p>
+            </section>
+            <section className="pb-12">
+              <h3 className="section-title text-lg font-extrabold">
+                How to cancel Gathering?
+              </h3>
+              <p className="pt-2">
+                If you would like to call off a gathering, you need to directly
+                contact to the organizer of gathering that you will join sending
+                a message by email.
+              </p>
+            </section>
+          </div>
+        </Card>
       </Wrapper>
     </>
   );
