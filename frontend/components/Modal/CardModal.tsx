4021a6dcc8b40d69af10a2d970124d77a167b891
KEENTO <handball0321@gmail.com>
2022-10-13 16:27:49 +0700

    Fixed the layout of GatheringCards

--- frontend/components/Modal/CardModal.tsx
+++ frontend/components/Modal/CardModal.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const CardModal = () => {
+interface CardProps {
+  isExpired: boolean;
+}
+
+const CardModal = ({ isExpired }: CardProps) => {
   return (
     <div className="absolute top-0 right-0 bottom-0 left-0 bg-slate-700 rounded-lg z-40 opacity-80">
       <div className="modal-container text-red-500 flex justify-center items-center h-full">
-        <p className="text-2xl font-semibold">This gathering is expired.</p>
+        <p className="text-2xl font-semibold">
+          {isExpired ? "Expired" : "Fully booked"}
+        </p>
       </div>
     </div>
   );
