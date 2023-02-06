034066778f93f4ee4071795ec5225e7636f687e1
KEENTO <handball0321@gmail.com>
2023-02-06 12:05:01 -0800

    Added Paper as a hero image to view pages

---
+++ frontend/components/Paper/HeroPaper.tsx
@@ -0,0 +1,14 @@
+import ImgPath from "../../public/static/heroPaper.jpg";
+
+const HeroPaper = () => {
+  return (
+    <div className="fixed top-0 right-0 bottom-0 left-0 opacity-40">
+      <img
+        src={ImgPath.src}
+        className="w-full h-2/3 lg:h-4/5 bg-center bg-cover"
+      />
+    </div>
+  );
+};
+
+export default HeroPaper;
