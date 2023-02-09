6e9ca6f907cd008f8195ddb754d120a9bc86c27a
KEENTO <handball0321@gmail.com>
2023-02-09 13:09:52 -0800

    Fixing page transition logic on NavBar

--- frontend/components/Title/Title.tsx
+++ frontend/components/Title/Title.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const Title = ({ title }: Props) => {
   return (
-    <h2 className="text-2xl pl-0.5 font-bold tracking-tighter text-left lg:text-center text-primary dark:text-red-400">
+    <h2 className="text-2xl pl-0.5 font-bold tracking-tighter text-left lg:text-center text-primary dark:text-red-400 [text-shadow:_0_2px_2px_rgb(255_255_255_/_70%)]">
       {title}
     </h2>
   );
